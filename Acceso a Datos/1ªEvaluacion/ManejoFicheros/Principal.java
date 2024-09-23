4daa06b313f1a1ad076b2ae571a1d0bdcfc455a5
Anxo55 <anxo.campos260204@gmail.com>
2024-09-23 12:14:03 +0200

    practica

--- Acceso a Datos/1ªEvaluacion/ManejoFicheros/Principal.java
+++ Acceso a Datos/1ªEvaluacion/ManejoFicheros/Principal.java
@@ -1,5 +1,7 @@
 package ManejoFicheros;
 
+import java.io.File;
+
 import javax.swing.JFileChooser;
 
 public class Principal {
@@ -7,9 +9,28 @@ public class Principal {
     public static void main(String[] args) {
 
         JFileChooser fileChooser = new JFileChooser();
-        int seleccion = fileChooser.showOpenDialog(fileChooser);
-        // int seleccion2 =  fileChooser.showOpenDialog(null);
-        
+        fileChooser.setFileSelectionMode(0);
+        int seleccion = fileChooser.showOpenDialog(null); // fileChooser, donde está el null
+        System.out.println(seleccion);
+
+        if(seleccion==0) {
+            File mifichero = fileChooser.getSelectedFile();
+            System.out.println(mifichero.getAbsolutePath());
+            System.out.println(mifichero.canRead());
+            System.out.println(mifichero.canWrite());
+            System.out.println(mifichero.getName());
+            if(mifichero.exists()) {
+                System.out.println("El fichero existe");
+                if (mifichero.isFile()) {
+                    System.out.println("Es un fichero");
+                }
+            }else {
+                System.out.println("El fichero no existe");
+                String[] listado = mifichero.list();
+            }
+        }
+
+                
     }
     
 }
---
+++ Acceso a Datos/1ªEvaluacion/ManejoFicheros/Principal2.java
@@ -0,0 +1,14 @@
+package ManejoFicheros;
+
+public class Principal2 {
+
+    public static void main(String[] args) {
+
+        String home = System.getProperty("user.home");
+        String separador = System.getProperty("file.separador");
+        System.out.println(home);
+        System.out.println(separador);
+        
+    }
+    
+}
